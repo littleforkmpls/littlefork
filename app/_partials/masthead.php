2938daedb311852e054104f9ebf1f0abc782ef02
Anthony Ticknor <tony.ticknor@gmail.com>
2022-04-14 23:13:52 -0500

    deecided to make it a one page site to speed up development

--- app/_partials/masthead.php
+++ app/_partials/masthead.php
@@ -17,23 +17,23 @@
                     <div class="nav__area" id="js-navToggleTarget">
                         <ul class="nav__area__list">
                             <li class="nav__area__list__item">
-                                <a <?php if ($initial_url_segment == 'services') { ?> class="isActive"<?php } ?>href="/services/">Services</a>
+                                <a href="#services">Services</a>
                             </li>
                             <li class="nav__area__list__item">
-                                <a <?php if ($initial_url_segment == 'case-studies') { ?> class="isActive"<?php } ?>href="/case-studies/">Case Studies</a>
+                                <a href="#case-studies">Case Studies</a>
                             </li>
                             <li class="nav__area__list__item">
-                                <a <?php if ($initial_url_segment == 'about') { ?> class="isActive"<?php } ?>href="/about/">About</a>
+                                <a href="#about">About</a>
                             </li>
                             <li class="nav__area__list__item">
-                                <a <?php if ($initial_url_segment == 'contact') { ?> class="isActive"<?php } ?>href="/contact/">Contact</a>
+                                <a href="#contact">Contact</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </div>
             <div class="masthead__section masthead__section--cta">
-                <a class="btn" href="/quote/">
+                <a class="btn" href="https://share.hsforms.com/1HswQpXJuQr6twqz1VwNdFgcmo7f">
                     <span class="btn__txt">Get a Quote</span>
                     <span class="btn__icon" aria-hidden="true">
                         &nbsp;&raquo;
